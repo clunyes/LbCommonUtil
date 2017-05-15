ad50c9f62271c3db349c67d1a7c2053aa0537aed
zhaokang <clunyes@126.com>
2017-05-15 17:04:01 +0800

    'codein'

---
+++ app/src/main/java/com/lb/lbcommonutil/util/NetWorkUtils.java
@@ -0,0 +1,82 @@
+package com.lb.lbcommonutil.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+import com.lb.lbcommonutil.app.BaseApplication;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * author：zhangxy
+ * time：2016/12/1  12:53.
+ * descr：网络管理工具
+ */
+
+public class NetWorkUtils {
+    /**
+     * 检查网络是否可用
+     *
+     * @param paramContext
+     * @return
+     */
+    public static boolean isNetConnected(Context paramContext) {
+        boolean i = false;
+        NetworkInfo localNetworkInfo = ((ConnectivityManager) paramContext
+                .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+        if ((localNetworkInfo != null) && (localNetworkInfo.isAvailable()))
+            return true;
+        return false;
+    }
+
+    /**
+     * 检测wifi是否连接
+     */
+    public static boolean isWifiConnected(Context context) {
+        boolean isWifiConnect = false;
+        ConnectivityManager cm = (ConnectivityManager) BaseApplication.getAppContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        // check the networkInfos numbers
+        NetworkInfo[] networkInfos = cm.getAllNetworkInfo();
+        for (int i = 0; i < networkInfos.length; i++) {
+            if (networkInfos[i].getState() == NetworkInfo.State.CONNECTED) {
+                if (networkInfos[i].getType() == ConnectivityManager.TYPE_MOBILE) {
+                    isWifiConnect = false;
+                }
+                if (networkInfos[i].getType() == ConnectivityManager.TYPE_WIFI) {
+                    isWifiConnect = true;
+                }
+            }
+        }
+        return isWifiConnect;
+    }
+
+    /**
+     * 检测3G是否连接
+     */
+    public static boolean is3gConnected(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (cm != null) {
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 判断网址是否有效
+     */
+    public static boolean isLinkAvailable(String link) {
+        Pattern pattern = Pattern.compile("^(http://|https://)?((?:[A-Za-z0-9]+-[A-Za-z0-9]+|[A-Za-z0-9]+)\\.)+([A-Za-z]+)[/\\?\\:]?.*$", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(link);
+        if (matcher.matches()) {
+            return true;
+        }
+        return false;
+    }
+}
